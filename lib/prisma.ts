448be39bbae5b15cfaf1b3e047b39a6353e1e0e0
Muhammad Ismail <mrismail433@gmail.com>
2026-03-23 17:03:14 +0500

    feat: Implement Prisma client singleton, configure Next.js build for ESLint and TypeScript, and automate Prisma generation with a postinstall script.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,12 +2,13 @@ import { PrismaClient } from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
 const connectionString = `${process.env.DATABASE_URL}`
 const pool = new Pool({ connectionString })
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const adapter = new PrismaPg(pool as any)
 
-export const prisma = new PrismaClient({ adapter, log: ['query'] })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter, log: ['query'] })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
